748177fa855cab10bf0e7e79000443e150cbaa4c
Vassil Kalkov <vassilkalkov@gmail.com>
2018-03-01 13:07:42 +0000

    Rewrite to use new ES features

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-var ds1820 = require('ds18x20');
-var raspiSensors = require('raspi-sensors');
+const ds1820 = require('ds18x20');
+const raspiSensors = require('raspi-sensors');
 
-var dht22 = new raspiSensors.Sensor({type: "DHT22", pin: 0X0}, "dht22");
-var light_sensor = new raspiSensors.Sensor({type: "TSL2561", address: 0X39}, "light_sensor");
+const dht22 = new raspiSensors.Sensor({type: "DHT22", pin: 0X0}, "dht22");
+const light_sensor = new raspiSensors.Sensor({type: "TSL2561", address: 0X39}, "light_sensor");
 
-ds1820.list(function (err, listOfDeviceIds) { console.log(listOfDeviceIds) })
-
-var fetchLightSensor =  new Promise(function(resolve, reject) {   
-  light_sensor.fetch(function(err, data) {
+const fetchLightSensor = new Promise((resolve, reject) => {   
+  light_sensor.fetch((err, data) => { 
     if(err) return reject(err.cause)
 
     resolve({name: data.sensor_name, data: data.value})
-   })
+  })
 })
 
-var fetchHumiditySensor = new Promise(function(resolve, reject) {
-  dht22.fetch(function (err, data) { 
+const fetchHumiditySensor = new Promise((resolve, reject) => {
+  dht22.fetch((err, data) => { 
     if(err) return reject(err) 
 
     resolve({name: data.sensor_name, data: data.value})
@@ -23,33 +21,30 @@ var fetchHumiditySensor = new Promise(function(resolve, reject) {
 })
 
 
-var fetchTemperatureSensor = new Promise(function(resolve, reject) { 
-  ds1820.getAll(function(err, data) {
+const fetchTemperatureSensor = new Promise((resolve, reject) => { 
+  ds1820.getAll((err, data) => {
     if(err) return reject(err) 
     
     var sensorsData = 
       Object.
         keys(data).
-	map(function(sensor_uuid, index) { 
-	  return {name: "ds1820_" + index, 
-		  uuid: sensor_uuid, 
-		  data: data[sensor_uuid]} 
+	map((uuid, index) => { 
+          return {name: `ds1820_${index}`, uuid: uuid, data: data[uuid]} 
 	})
     
     resolve(sensorsData)
   })
 })
 
-var publish = function() {
+const publish = () => {
   Promise.
     all([fetchHumiditySensor, fetchLightSensor, fetchTemperatureSensor]).
-    then(function(results) { 
-      //ES2015 arrays = arrays.reduce((a, b) => a.concat(b), [])
-      var sensors = [].concat.apply([], results)
+    then(results => { 
+      const sensors = results.reduce((a, b) => a.concat(b), [])
       console.log(sensors) 
     }).
-    catch(function(error) { console.error(error) })
+    catch(error => console.error(error))
 }
 
-setInterval(function() { publish() }, 2000)
+setInterval(() => publish(), 2000)
 

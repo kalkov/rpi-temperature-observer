67bfe6446e67d7ead2244b6deaf8768cb58c855d
Vassil Kalkov <vassilkalkov@gmail.com>
2018-03-12 12:45:43 +0000

    Add support for 3 DHT22 sensors

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const ds1820 = require('ds18x20');
 const raspiSensors = require('raspi-sensors');
 
 const dht22_0 = new raspiSensors.Sensor({type: "DHT22", pin: 0X0}, "dht22_0");
+const dht22_1 = new raspiSensors.Sensor({type: "DHT22", pin: 0X1}, "dht22_1");
+const dht22_2 = new raspiSensors.Sensor({type: "DHT22", pin: 0X2}, "dht22_2");
+
 const light_sensor = new raspiSensors.Sensor({type: "TSL2561", address: 0X39}, "light_sensor");
 
 const username = config.get('username')
@@ -13,13 +16,15 @@ const mqtt_url = config.get('mqtt_url')
 const device = config.get('device')
 const clientId = `${username}/${device}`
 const topic = 'api/v1/sensors'
-const publishInterval = 30000
+const publishInterval = 7000
 
 const options = {clientId: clientId, username: username, password: token}
 const client = mqtt.connect(mqtt_url, options)
 
 const dhtNames = {
-  dht22_0: {Temperature: 'dht22_0_temp', Humidity: 'dht22_0_hum'}
+  dht22_0: {Temperature: 'dht22_0_temp', Humidity: 'dht22_0_hum'}, 
+  dht22_1: {Temperature: 'dht22_1_temp', Humidity: 'dht22_1_hum'},
+  dht22_2: {Temperature: 'dht22_2_temp', Humidity: 'dht22_2_hum'}
 }
 
 console.log(`Connecting to: ${mqtt_url} with {username: ${username}, token: ${token}}`.yellow)
@@ -37,21 +42,23 @@ function fetchLightSensor() {
   })
 }
 
-function fetchHumiditySensor() {
+function fetchHumiditySensor(dht) {
   return new Promise((resolve, reject) => {
     var oldValue
     
-    dht22_0.fetch((err, data) => { 
+    dht.fetch((err, data) => { 
       if(err) return reject(err) 
-      
+
       // HACK .fetch() invokes two functions - one for temperature and one for humidity, each one returns an object.  	    
       if (!oldValue) { 
         oldValue = data 
-      } else {
+      } else if(oldValue.value || data.value) {
         resolve([
 	  {name: dhtNames[oldValue.sensor_name][oldValue.type], data: oldValue.value}, 
 	  {name: dhtNames[data.sensor_name][data.type], data: data.value}
 	])
+      } else {
+	resolve(null)
       }
     }) 
   })
@@ -61,12 +68,14 @@ function fetchTemperatureSensors() {
   return new Promise((resolve, reject) => { 
     ds1820.getAll((err, data) => {
       if(err) return reject(err) 
-    
+      
       var sensorsData = 
         Object.
           keys(data).
 	  map((uuid, index) => { 
-            return {name: `ds1820_${index}`, uuid: uuid, data: data[uuid]} 
+            if(data[uuid] !== 85) {
+              return{name: `ds1820_${index}`, uuid: uuid, data: data[uuid]}
+            }
 	  })
     
       resolve(sensorsData)
@@ -76,9 +85,9 @@ function fetchTemperatureSensors() {
 
 const fetchAndPublish = () => {
   Promise.
-    all([fetchHumiditySensor(), fetchLightSensor(), fetchTemperatureSensors()]).
+    all([fetchHumiditySensor(dht22_0), fetchHumiditySensor(dht22_1), fetchHumiditySensor(dht22_2), fetchLightSensor(), fetchTemperatureSensors()]).
     then(results => { 
-      const sensors = results.reduce((a, b) => a.concat(b), [])
+      const sensors = results.reduce((a, b) => a.concat(b), []).filter((result) => result)
       const payload = {device_name: device, sensors: sensors}
       const time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
       
@@ -88,9 +97,6 @@ const fetchAndPublish = () => {
     }).
     catch(error => console.error(error))
 }
-
-
-fetchAndPublish()
 
 setInterval(() => fetchAndPublish(), publishInterval)
 
